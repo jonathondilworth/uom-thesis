3d34092507582e7ff48b9ba8573fbec369cf55e0
Jonathon Dilworth <jonathon.dilworth@postgrad.manchester.ac.uk>
2025-09-07 13:42:50 +0100

    typo

--- scripts/build_ont_data.sh
+++ scripts/build_ont_data.sh
@@ -27,9 +27,9 @@ echo ""
 
 echo "[Step 1/1] Building OnT dataset for training OnT models as used within the paper ... "
 
-#echo "" | conda run -n "$AUTO_ENV_NAME" --no-capture-output python ./lib/OnT/normalization/modELNormalize.py \
-#  --input ./data/snomedct-international.owl \
-#  --output ./data/ont_dataset
+echo "" | conda run -n "$AUTO_ENV_NAME" --no-capture-output python ./lib/OnT/normalization/modELNormalize.py \
+ --input ./data/snomedct-international.owl \
+ --output ./data/ont_dataset
 
 echo "[Step 2/2] Handling role_inverse.json edge case (creating a single obj in role_inverse.json file)..."
 
